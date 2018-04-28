41313698977c64e02b3bbb8613498ebb6d53919a
lijion <lijion@indiana.edu>
2018-04-28 16:23:22 +1000

    only contract owner(who deploy the contract) can withdraw money from the contract.

--- contracts/transaction.sol
+++ contracts/transaction.sol
@@ -4,7 +4,12 @@ contract Transaction {
 
    
    uint public constant price = 1 finney;
+   address owner;
    event LogTransfer(address sender, address to, uint amount, uint productId);
+    
+    function Transaction(){
+        owner = msg.sender;
+    }
     
     function transfer(address to, uint amount, uint productId) payable {
         if (msg.value < (amount * price)) {
@@ -12,6 +17,11 @@ contract Transaction {
         }
         to.transfer(amount * price);
         LogTransfer(msg.sender, to, amount, productId);
+    }
+    
+    function withdraw() {
+        require(msg.sender == owner);
+        msg.sender.transfer(this.balance);
     }
     
 }

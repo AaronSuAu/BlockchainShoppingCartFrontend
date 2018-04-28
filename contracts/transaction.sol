20b6f16376bf3dbf2a5bfb0b41b0e49786abee60
lijion <lijion@indiana.edu>
2018-04-28 15:42:35 +1000

    update smart contract, eliminate deposit and withdraw function.

--- contracts/transaction.sol
+++ contracts/transaction.sol
@@ -2,30 +2,14 @@ pragma solidity ^0.4.11;
 
 contract Transaction {
 
-   mapping (address => uint) public balances;
+   
    uint public constant price = 1 finney;
    event LogTransfer(address sender, address to, uint amount, uint productId);
     
-    function deposit(uint amount) payable  {
-        if (msg.value != (amount * price)) {
+    function transfer(address to, uint amount, uint productId) payable {
+        if (msg.value < (amount * price)) {
             throw;
         }
-        balances[msg.sender] += amount;
-    }
-
-    function withdraw(uint amount) {
-        if (balances[msg.sender] < amount) {
-            throw;
-        }
-        balances[msg.sender] -= amount;
-        msg.sender.transfer(amount * price);
-    }
-
-    function transfer(address to, uint amount, uint productId) {
-        if (balances[msg.sender] < amount) {
-            throw;
-        }
-        balances[msg.sender] -= amount;
         to.transfer(amount * price);
         LogTransfer(msg.sender, to, amount, productId);
     }
